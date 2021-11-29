43ff758ee8ec55ba84333939de2df6059a955f88
이동현 <kura1031@naver.com>
2021-11-29 10:08:40 +0900

    캔버스 OnPaint 이벤트 예제

---
+++ Section8/CanvasOnPaintProject.dpr
@@ -0,0 +1,13 @@
+program CanvasOnPaintProject;
+
+uses
+  Forms,
+  CanvasOnPaint in 'CanvasOnPaint.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
